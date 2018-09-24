da5c074fccdff3b0a6112a14ac0d3992bb845d2b
Alexander Schell <S00190272@mail.itsligo.ie>
2018-09-24 09:46:52 +0100

    Add files via upload

---
+++ 3DGameEngineProgramming/3DGameEngineProgramming/3DGameEngineProgramming/Game1.cs
@@ -0,0 +1,353 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _3DGameEngineProgramming
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+
+        //Camera
+        Matrix view;
+        Matrix projection;
+
+        //Matrix
+        Matrix world = Matrix.Identity;
+
+        // Colour Verticies
+        VertexPositionColor[] colourVerticies;
+
+        // Texture Verticies
+        VertexPositionTexture[] texturedVertices;
+
+        //Normal Verticies
+        VertexPositionNormalTexture[] normalVertices;
+        // Texture
+        Texture2D uvTexture;
+
+        // Effect
+        BasicEffect effect;
+
+        // 16 bit integer
+        int[] indicies;
+
+        // buffers
+
+        VertexBuffer vBuffer;
+        IndexBuffer iBuffer;
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+
+            graphics.PreferredBackBufferWidth = 1280; // Screen Width
+            graphics.PreferredBackBufferHeight = 720;   // Screen Height
+
+            IsMouseVisible = true; // Makes Mouse Visable
+            TargetElapsedTime = new System.TimeSpan(0, 0, 0, 0, 16); // 
+
+
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            // Creates our cameras View position
+            view = Matrix.CreateLookAt(new Vector3(0, 0, 5),
+                                       new Vector3(0, 0, -1), 
+                                       Vector3.Up);                 
+
+            // Creates our field of view and perspective
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45),
+                                                             GraphicsDevice.DisplayMode.AspectRatio,
+                                                             0.1f,
+                                                             1000f);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+
+             SetupColourVerticies();
+            // SetupTextureVerticies();
+            //SetupNormalVertices();
+        }
+
+        void SetupColourVerticies()
+        {
+            colourVerticies = new VertexPositionColor[12];
+
+            //BL
+            colourVerticies[0] = new VertexPositionColor(new Vector3(-1, -1, 0), 
+                                                         Color.Purple);
+            //TL
+            colourVerticies[1] = new VertexPositionColor(new Vector3(-1, 1, 0), 
+                                                         Color.Purple);
+            //BR
+            colourVerticies[2] = new VertexPositionColor(new Vector3(1, -1, 0), 
+                                                         Color.Purple);
+        
+            //TR
+            colourVerticies[3] = new VertexPositionColor(new Vector3(1, 1, 0),
+                                                        Color.Purple);
+           //------------------------------------------------------------------------
+            //BR
+            colourVerticies[4] = new VertexPositionColor(new Vector3(-1, -1, -2),
+                                                        Color.Purple);
+            //TL
+            colourVerticies[5] = new VertexPositionColor(new Vector3(1, -1, -2),
+                                                        Color.Purple);           
+
+            colourVerticies[6] = new VertexPositionColor(new Vector3(1, 1, -2),
+                                                        Color.Purple);
+
+            colourVerticies[7] = new VertexPositionColor(new Vector3(-1, 1, -2),
+                                                        Color.Purple);
+
+
+            indicies = new int[36];
+
+            // assigning triangle to array
+            // center tri
+
+            indicies[0] = 0;
+            indicies[1] = 1;
+            indicies[2] = 2;   
+
+            indicies[3] = 2;
+            indicies[4] = 1;
+            indicies[5] = 3;
+            
+            // back tri
+
+            indicies[6] = 4;
+            indicies[7] = 5;
+            indicies[8] = 6;
+
+            indicies[9] = 7;
+            indicies[10] = 4;
+            indicies[11] = 6;
+
+            //left tri
+            indicies[12] = 3;
+            indicies[13] = 6;
+            indicies[14] = 5;
+
+            indicies[15] = 2;
+            indicies[16] = 3;
+            indicies[17] = 5;
+
+            // Right tri
+            indicies[18] = 0;
+            indicies[19] = 4;
+            indicies[20] = 1;            
+            
+            indicies[21] = 4;
+            indicies[22] = 7;
+            indicies[23] = 1;
+
+            // top tri
+            indicies[24] = 1;
+            indicies[25] = 7;
+            indicies[26] = 3;
+
+            indicies[27] = 7;
+            indicies[28] = 6;
+            indicies[29] = 3;
+
+            // Bot tri
+            indicies[30] = 4;
+            indicies[31] = 0;
+            indicies[32] = 5;
+
+            indicies[33] = 2;
+            indicies[34] = 5;
+            indicies[35] = 0;
+
+
+            vBuffer = new VertexBuffer(GraphicsDevice,
+                                       VertexPositionColor.VertexDeclaration,
+                                       colourVerticies.Length, BufferUsage.WriteOnly);
+
+            vBuffer.SetData(colourVerticies);
+
+            iBuffer = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, 
+                                      indicies.Length, BufferUsage.WriteOnly);
+            iBuffer.SetData(indicies);
+
+
+            effect = new BasicEffect(GraphicsDevice);
+            effect.VertexColorEnabled = true;
+            effect.TextureEnabled = false;
+
+            RasterizerState state = new RasterizerState();
+            state.FillMode = FillMode.WireFrame;
+            state.CullMode = CullMode.None;
+
+
+            //GraphicsDevice.RasterizerState = state;
+
+        }
+
+        void SetupTextureVerticies()
+        {
+            // amount of verticies
+            texturedVertices = new VertexPositionTexture[6];
+
+
+            // Bl
+            texturedVertices[0] = new VertexPositionTexture(new Vector3(-1, -1, 0),
+                                                            new Vector2(0, 1));
+            // Tl
+            texturedVertices[1] = new VertexPositionTexture(new Vector3(-1, 1, 0),
+                                                            new Vector2(0, 0));
+            // BR
+            texturedVertices[2] = new VertexPositionTexture(new Vector3(1, -1, 0),
+                                                            new Vector2(1, 1));
+            //-------------------------------------------------------------------
+           
+            // Tr
+            texturedVertices[3] = new VertexPositionTexture(new Vector3(1, 1, 0),
+                                                            new Vector2(1, 0));
+            // BR
+            texturedVertices[4] = new VertexPositionTexture(new Vector3(1, -1, 0),
+                                                            new Vector2(1, 1));
+            // Tl
+            texturedVertices[5] = new VertexPositionTexture(new Vector3(-1, 1, 0),
+                                                            new Vector2(0, 0));
+
+
+            // load texture
+            uvTexture = Content.Load<Texture2D>("uv_texture");
+
+
+
+            effect = new BasicEffect(GraphicsDevice);
+            effect.TextureEnabled = true;
+            effect.Texture = uvTexture;
+
+        }
+
+        void SetupNormalVertices()
+        {
+            normalVertices = new VertexPositionNormalTexture[4];
+
+            normalVertices[0] = new VertexPositionNormalTexture(new Vector3(-1, -1, 0), 
+                                                                Vector3.Forward,
+                                                                new Vector2(0, 1));
+
+            normalVertices[1] = new VertexPositionNormalTexture(new Vector3(-1, 1, 0),
+                                                                Vector3.Forward,
+                                                                new Vector2(0, 0));
+
+            normalVertices[2] = new VertexPositionNormalTexture(new Vector3(1, -1, 0),
+                                                               Vector3.Forward,
+                                                                new Vector2(1, 1));
+
+            normalVertices[3] = new VertexPositionNormalTexture(new Vector3(-1, -1, -1),
+                                                                Vector3.Forward,
+                                                                new Vector2(1, 0));
+/*
+            normalVertices[4] = new VertexPositionNormalTexture(new Vector3(1, -1, 0),
+                                                                Vector3.Forward,
+                                                                new Vector2(1, 1));
+
+            normalVertices[5] = new VertexPositionNormalTexture(new Vector3(-1, 1, 0),
+                                                                Vector3.Forward,
+                                                                new Vector2(0, 0));
+                                                                */
+
+            // createing a new indicie array
+
+            indicies = new int[6];
+
+            // assigning triangle to array
+            // center tri
+            indicies[0] = 0;
+            indicies[1] = 1;
+            indicies[2] = 2;
+            
+            //  left tri
+            indicies[3] = 2;
+            indicies[4] = 1;
+            indicies[5] = 3;
+            
+
+            // load texture
+            uvTexture = Content.Load<Texture2D>("uv_texture");
+
+            effect = new BasicEffect(GraphicsDevice);
+            effect.TextureEnabled = true;
+            effect.Texture = uvTexture;
+
+            effect.EnableDefaultLighting();
+            effect.PreferPerPixelLighting = true;
+
+            effect.DirectionalLight0.Enabled = true;
+            effect.DirectionalLight1.Enabled = false;
+            effect.DirectionalLight2.Enabled = false;
+
+            effect.DirectionalLight0.Direction = Vector3.Backward;
+        }
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+       
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        void DrawTexturedVertices()
+        {
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawUserIndexedPrimitives<VertexPositionColor>(PrimitiveType.TriangleList, colourVerticies, 0, colourVerticies.Length, indicies, 0, indicies.Length / 3);
+            }
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+
+            GraphicsDevice.SetVertexBuffer(vBuffer);
+            GraphicsDevice.Indices = iBuffer;
+
+            GraphicsDevice.Clear(Color.Black);
+
+            effect.View = view;
+            effect.Projection = projection;
+
+            effect.World = world;
+
+           
+            world *=  Matrix.CreateRotationX(MathHelper.ToRadians(-1f));
+            world *=  Matrix.CreateRotationY(MathHelper.ToRadians(-1f));
+
+            /*
+            // Matrix transformation
+            world *= Matrix.CreateTranslation(new Vector3(0, 0, -1));
+            
+            world *= Matrix.CreateScale(1);
+           */
+
+
+            DrawTexturedVertices();
+            
+
+            base.Draw(gameTime);
+        }
+    }
+}
